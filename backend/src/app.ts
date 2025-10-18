cc9eaa3908dca1f768db40c321543bdd070d0104
bonekyaw <phonenai2014@gmail.com>
2025-10-18 12:08:35 +0630

    prisma

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,17 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
 import router from "./routes";
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "src/views");
+// app.set("view engine", "ejs");
+// app.set("views", "src/views");
 
-app.use(express.urlencoded({ extended: true }));
+app
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(cookieParser());
+
 app.use(express.static("public"));
 
 app.use(router);
