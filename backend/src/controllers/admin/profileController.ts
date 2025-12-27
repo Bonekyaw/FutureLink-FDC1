f650d039d49ef362430b519c0e0f47bdcc4cb575
bonekyaw <phonenai2014@gmail.com>
2025-12-27 10:55:30 +0630

    optimized file

--- backend/src/controllers/admin/profileController.ts
+++ backend/src/controllers/admin/profileController.ts
@@ -1,4 +1,7 @@
 // This is for testing purposes only
+import sharp from "sharp";
+import path from "path";
+
 import { Request, Response, NextFunction } from "express";
 interface AuthenticatedRequest extends Request {
   userId?: number;
@@ -11,16 +14,32 @@ export const profileUploadTest = async (
 ) => {
   const userId = req.userId;
   const file = req.file;
+  // console.log("File Upload", file);
 
   if (!file) {
     return res.status(400).json({ message: "No file uploaded." });
   }
 
-  res
-    .status(200)
-    .json({
-      message: "File uploaded successfully.",
-      filePath: file.path,
-      userId,
-    });
+  const splitFileName = file.originalname.split(".")[0];
+
+  const optimizeFilePath = path.join(
+    __dirname,
+    "../../..",
+    "/uploads/images/optimized-" + splitFileName + ".webp"
+  );
+
+  try {
+    await sharp(req.file?.buffer)
+      .resize(200, 200)
+      .webp({ quality: 50 })
+      .toFile(optimizeFilePath);
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.status(200).json({
+    message: "File uploaded successfully.",
+    // filePath: file.path,
+    userId,
+  });
 };
--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import upload from "../middleware/uploadFile";
+import upload, { uploadMemory } from "../middleware/uploadFile";
 import { profileUploadTest } from "../controllers/admin/profileController";
 import { auth } from "../middleware/auth";
 
 const router = express.Router();
 
-router.post("/profile", auth, upload.single("avatar"), profileUploadTest);
+router.post("/profile", auth, uploadMemory.single("avatar"), profileUploadTest);
 
 export default router;
