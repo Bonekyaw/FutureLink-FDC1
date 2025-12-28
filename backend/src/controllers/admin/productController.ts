88a8945be008618bcd3c7222d86150b56c29fbd6
bonekyaw <phonenai2014@gmail.com>
2025-12-28 12:06:08 +0630

    product

--- backend/src/controllers/admin/productController.ts
+++ backend/src/controllers/admin/productController.ts
@@ -2,11 +2,32 @@ import { body, validationResult } from "express-validator";
 import { Request, Response, NextFunction } from "express";
 import { createError } from "../../utils/error";
 import { errorCode } from "../../config";
+import path from "path";
+import { unlink } from "fs/promises";
+import { findUserById } from "../../models/userRepository";
+import sharp from "sharp";
 
 interface AuthenticatedRequest extends Request {
   userId?: number;
   files?: any;
 }
+
+const removeFiles = async (originalFileNames: string[]) => {
+  // Implement file removal logic here, e.g., using fs.unlink
+  try {
+    for (const originalFileName of originalFileNames) {
+      const originalFilePath = path.join(
+        __dirname,
+        "../../..",
+        "/uploads/images/" + originalFileName
+      );
+      await unlink(originalFilePath);
+      console.log(`Deleted file: ${originalFilePath}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const createProduct = [
   body("name", "Name is required").trim().notEmpty().escape(),
@@ -31,8 +52,63 @@ export const createProduct = [
     if (errors.length > 0) {
       if (req.files && req.files.length > 0) {
         // Delete uploaded files if validation fails
+        const originalFiles = req.files.map((file: any) => file.filename);
+        await removeFiles(originalFiles);
       }
       return next(createError(errors[0].msg, 400, errorCode.invalid));
     }
+
+    if (req.files && req.files.length === 0) {
+      return next(
+        createError("At least one image is required", 400, errorCode.invalid)
+      );
+    }
+
+    // Authorisation check
+    const userId = req.userId;
+    const user = await findUserById(userId!);
+    if (user?.role !== "ADMIN") {
+      return next(
+        createError("This action is not allowed", 403, errorCode.unauthorised)
+      );
+    }
+
+    const {
+      name,
+      description,
+      price,
+      discount,
+      inventory,
+      category,
+      type,
+      tags,
+    } = req.body;
+    const files = req.files;
+
+    // Optimize file
+    await Promise.all(
+      files.map(async (file: any) => {
+        // console.log("Uploaded file:", file.filename);
+        const splitFileName = file.filename.split(".")[0];
+        const optimizeFilePath = path.join(
+          __dirname,
+          "../../..",
+          "/uploads/optimize/" + splitFileName + ".webp"
+        );
+
+        try {
+          return await sharp(file.path)
+            .resize(835, 577)
+            .webp({ quality: 80 })
+            .toFile(optimizeFilePath);
+        } catch (error) {
+          console.log(error);
+        }
+      })
+    );
+
+    const originalFileNames = files.map((file: any) => ({
+      url: file.filename,
+    }));
   },
 ];
--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -2,9 +2,11 @@ import express from "express";
 import upload, { uploadMemory } from "../middleware/uploadFile";
 import { profileUploadTest } from "../controllers/admin/profileController";
 import { auth } from "../middleware/auth";
+import { createProduct } from "../controllers/admin/productController";
 
 const router = express.Router();
 
 router.post("/profile", auth, uploadMemory.single("avatar"), profileUploadTest);
+router.post("/product/create", auth, upload.array("images", 5), createProduct);
 
 export default router;
